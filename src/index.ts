373dd5f918ccabb728aaaa9775cb68103d5d1bb7
proustibat <jennifer.proust@gmail.com>
2021-07-08 09:49:31 +0000

    Deploying to gh-pages from master @ 5cf833ed8f06b3c877d26d249bde7540223aa94e 🚀

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,27 @@ console.log('Script started successfully');
 // WA.openCoWebSite('https://workadventu.re');
 // WA.sendChatMessage('Hello you!', 'Tpacq bot');
 
-
 /**
  * Opening a popup
  */
 type Popup = {
-    id: number;
-    close(): void;
-}
-const zoneName = 'music'
+  close(): void;
+};
+const zoneName = 'music';
 let helloWorldPopup: Popup;
 const onEnterMusic = () =>
-    WA.openPopup("popupRectangle", 'You are listening to "Regia" a music from Amenofis', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => popup.close()
-    }]);
+  WA.openPopup(
+    'popupRectangle',
+    'You are listening to "Regia" a music from Amenofis',
+    [
+      {
+        label: 'Close',
+        className: 'primary',
+        callback: popup => popup.close(),
+      },
+    ],
+  );
 helloWorldPopup = WA.onEnterZone(zoneName, onEnterMusic) as unknown as Popup;
 const closePopup = helloWorldPopup.close;
 
-// Close the popup when we leave the zone.
 WA.onLeaveZone(zoneName, closePopup);
