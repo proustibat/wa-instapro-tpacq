2a6bf8743167dd17f3f0e3112dec522351194afa
proustibat <jennifer.proust@gmail.com>
2021-07-08 09:25:08 +0000

    Deploying to gh-pages from master @ a0e085f78af25e45eb133f2450e1a2974dbe34e5 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully")})();
+(()=>{"use strict";let e;console.log("Script started successfully"),WA.sendChatMessage("Hello you!","Tpacq bot"),e=WA.onEnterZone("music",(()=>WA.openPopup("popupRectangle",'You are listening to "Regia" a music from Amenofis',[{label:"Close",className:"primary",callback:e=>e.close()}])));const o=e.close;WA.onLeaveZone("music",o)})();
 //# sourceMappingURL=script.js.map
