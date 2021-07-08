a0e085f78af25e45eb133f2450e1a2974dbe34e5
proustibat <jennifer.proust@gmail.com>
2021-07-08 11:24:16 +0200

    script

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,31 @@
 
 // You can write your WorkAdventure script here, if any.
 // The "WA" global object is available from anywhere.
-
 console.log('Script started successfully');
 // WA.openCoWebSite('https://workadventu.re');
+WA.sendChatMessage('Hello you!', 'Tpacq bot');
+
+
+/**
+ *
+ */
+
+type Popup = {
+    id: number;
+    close(): void;
+}
+
+let helloWorldPopup: Popup;
+
+const onEnterMusic = () => {
+    return WA.openPopup("popupRectangle", 'You are listening to "Regia" a music from Amenofis', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => popup.close()
+    }]);
+};
+helloWorldPopup = WA.onEnterZone('music', onEnterMusic) as unknown as Popup;
+const closePopup = helloWorldPopup.close;
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('music', closePopup);
