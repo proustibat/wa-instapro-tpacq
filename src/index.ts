c18878434fd0068d31ca8974c007b9e558a87009
proustibat <jennifer.proust@gmail.com>
2021-07-12 19:42:47 +0200

    interactions: tv, music, meeting

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,8 @@ console.log('Script started successfully');
 // WA.sendChatMessage('Hello you!', 'Tpacq bot');
 
 /**
- * Opening a popup
+ * TYPES
  */
-const { openPopup, onEnterZone, onLeaveZone } = WA;
 type Popup = {
   close: () => void;
 };
@@ -25,29 +24,119 @@ type ButtonDescriptor = {
     | 'disabled';
   callback: ButtonClickedCallback;
 };
-const zone = 'music';
-const popupRectangle = 'musicPopup';
-
-const {
-  targetObject,
-  message,
-  buttons,
-}: {
-  targetObject: string;
-  message: string;
-  buttons: ButtonDescriptor[];
-} = {
-  targetObject: popupRectangle,
-  message: 'You are listening to "Regia" a music from Amenofis',
-  buttons: [
-    {
-      label: 'Close',
-      className: 'primary',
-      callback: popup => popup.close(),
-    },
-  ],
+declare type SoundConfig = {
+  volume?: number;
+  loop?: boolean;
+  rate?: number;
+  detune?: number;
+  delay?: number;
+  seek?: number;
+  mute?: boolean;
 };
+declare class Sound {
+  play(soundConfig: SoundConfig | undefined): void;
+  stop(): void;
+}
 
-const onEnterMusic = () => openPopup(targetObject, message, buttons);
-const closePopup = (onEnterZone(zone, onEnterMusic) as unknown as Popup).close;
-onLeaveZone(zone, closePopup);
+/**
+ * WorkAdventureApi
+ */
+const { openPopup, onEnterZone, onLeaveZone, loadSound } = WA as any;
+
+/**
+ * SOUNDS MANAGEMENT
+ */
+const soundConfig: SoundConfig = {
+  volume: 1,
+  loop: false,
+  rate: 1,
+  detune: 1,
+  delay: 0,
+  seek: 0,
+  mute: false,
+};
+const trackPath = 'assets/music';
+const tracks = [
+  {
+    title: 'Worakls - Elea',
+    fileName: 'worakls_elea.mp3',
+  },
+  {
+    title: 'Parra for Cuva - Luhya (Original Mix)',
+    fileName: 'parra-for-cuva_luhya_original-mix.mp3',
+  },
+  {
+    title: 'Cymande - Brothers On The Slide',
+    fileName: 'cymande_brothers-on-the-slide.mp3',
+  },
+  {
+    title: 'Zeigest - Wrecked Metal',
+    fileName: 'zeigest_wrecked-metal.mp3',
+  },
+  {
+    title: 'Fadoul - Bsslama Habiti',
+    fileName: 'fadoul_bsslama-habiti.mp3',
+  },
+  {
+    title: 'The Knife - Pass This On (Deep Cuts 03)',
+    fileName: 'the-knife_pass-this-on.mp3',
+  },
+  {
+    title: 'Kompromat - Niemand',
+    fileName: 'kompromat_niemand.mp3',
+  },
+  {
+    title: 'Peter Schilling - Völlig losgelöst',
+    fileName: 'peter-schilling_vollig-losgelost.mp3',
+  },
+  {
+    title: 'Galt MacDermot - Let The Sunshine In',
+    fileName: 'galt-mac-dermot_let-the-sunshine-in.mp3',
+  },
+  {
+    title: 'Amenofis - Regia',
+    fileName: 'amenofis_regia--original-mix.mp3',
+  },
+];
+
+let currentSound: Sound | null;
+
+const onSelectMusic: (fileName: string) => ButtonClickedCallback =
+  fileName => async popup => {
+    if (!currentSound) {
+      currentSound = await loadSound(`${trackPath}/${fileName}`);
+    }
+    if (currentSound) {
+      currentSound.play(soundConfig);
+    }
+    popup.close();
+  };
+
+/**
+ * JUKEBOX POPUP
+ */
+let currentJukeboxPopup: Popup | null = null;
+const zone = 'danceFloor';
+const popupRectangle = 'jukebox';
+const popupMessage = 'Which music do you wanna listen to?';
+const buttons = tracks.map(track => ({
+  label: track.title,
+  className: 'normal',
+  callback: onSelectMusic(track.fileName),
+}));
+
+onEnterZone(zone, () => {
+  if (!currentJukeboxPopup) {
+    currentJukeboxPopup = openPopup(popupRectangle, popupMessage, buttons);
+  }
+});
+onLeaveZone(zone, () => {
+  if (currentSound) {
+    currentSound.stop();
+    currentSound = null;
+  }
+  if (currentJukeboxPopup) {
+    currentJukeboxPopup.close();
+    currentJukeboxPopup = null;
+  }
+});
